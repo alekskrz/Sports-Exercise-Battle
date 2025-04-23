557dc6a3de672622e56fbaeaa174066bc3aa0ce9
alekskrz <acakrzan@gmail.com>
2025-04-23 11:33:10 +0200

    Adding the right files this time

---
+++ Sports-Exercise-Battle/BusinessLayer/Models/Push-Up-Record.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sports_Exercise_Battle.BusinessLayer.Models
+{
+    internal class Push_Up_Record
+    {
+        public string User {  get; set; }
+        public int Count { get; set; }
+        public int Duration_of_exercise { get; set; }
+    }
+}
---
+++ Sports-Exercise-Battle/BusinessLayer/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sports_Exercise_Battle.BusinessLayer.Models
+{
+    public class User
+    {
+        public string Username { get; }
+        public string Password { get; set; }
+        
+        public int Elo {  get; set; }
+
+        public User(string username, string password) 
+        {  
+            Username = username; 
+            Password = password;
+        }
+    }
+}
